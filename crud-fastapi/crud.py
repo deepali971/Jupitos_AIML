6c34e9060d657671d7c0462346ce1b587f986822
deepali971 <102285175+deepali971@users.noreply.github.com>
2025-04-22 23:23:53 +0530

    Add files via upload

---
+++ crud-fastapi/crud.py
@@ -0,0 +1,41 @@
+from sqlalchemy.orm import Session
+from models import User
+from datetime import date
+
+def get_user(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(User).filter(User.username == username).first()
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(User).offset(skip).limit(limit).all()
+
+def create_user(db: Session, user):
+    db_user = User(**user.dict())
+    db_user.calculate_age()
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def update_user(db: Session, user_id: int, user_update):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if db_user:
+        update_data = user_update.dict(exclude_unset=True)
+        for key, value in update_data.items():
+            setattr(db_user, key, value)
+        db_user.calculate_age()
+        db.commit()
+        db.refresh(db_user)
+    return db_user
+
+def delete_user(db: Session, user_id: int):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if db_user:
+        db.delete(db_user)
+        db.commit()
+    return db_user
